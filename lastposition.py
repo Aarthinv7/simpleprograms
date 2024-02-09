93b20500454ce1c1b1c351e75fffa4871d46065d
Aarthinv7 <156234406+Aarthinv7@users.noreply.github.com>
2024-02-09 21:16:56 +0530

    Delete lastposition.py

--- lastposition.py
+++
@@ -1,7 +0,0 @@
-String="Emma is a data scientist who knows python.Emma works at google"
-substring="Emma"
-str_index = -1
-for i in range(len(String)):
-    if String[i:i+len(substring)] == substring:
-        str_index = i
-print(str_index)
